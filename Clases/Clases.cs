8d3cb39f0d36b53d90b5717e5f14cc219c664843
Alex04 <alexmonterogaray@gmail.com>
2025-01-29 22:48:13 -0500

    Exercise Poly add GetCoef, ToString and grade

--- Clases/Clases.cs
+++ Clases/Clases.cs
@@ -5,54 +5,68 @@ using System.Text;
 class Poly
 {
     private int[] polinomio;
-    public int[] Polinomio{get => polinomio;}
+    public int[] Polinomio { get => polinomio; }
     private int gradePoly;
     public int GradePoly
     {
-        get=>gradePoly;
+        get => gradePoly;
     }
     public int[] NewPoly(int[] polinomio)
     {
-      this.polinomio=polinomio;
-      this.gradePoly=Grade(polinomio);
-      return polinomio;
+        this.polinomio = polinomio;
+        this.gradePoly = Grade(polinomio);
+        return polinomio;
     }
     public Poly(int[] polinomio)
     {
-        this.polinomio=polinomio;
-        this.gradePoly=Grade(polinomio);
+        this.polinomio = polinomio;
+        this.gradePoly = Grade(polinomio);
     }
     public void addCoeficiente(int[] coeficiete)
     {
-       if(coeficiete.Length != this.gradePoly) throw new Exception();
-       polinomio=coeficiete;
+        if (coeficiete.Length != this.gradePoly) throw new Exception();
+        polinomio = coeficiete;
     }
     private int Grade(int[] polinomio)
     {
-      int pos=polinomio.Length-1;
-      if(polinomio[pos] != 0) return polinomio.Length;
-      else if(pos-1>=0)
-        for(int i=pos-1;i>=0;i--) 
-        { 
-            if(polinomio[pos]!=0) return pos;
+        int pos = polinomio.Length - 1;
+        if (polinomio[pos] != 0) return polinomio.Length-1;
+        else if(pos-1>=0)   
+            for (int i = pos=1; i >= 0; i--)
+            {
+                if (polinomio[i] != 0) return i;
+                 
+            }
         
-        }
-       return 0; 
+        return 0;
     }
-    public int GetCoef(int k)=>polinomio[k];
-    public int Degrade()
+    public int GetCoef(int k)
     {
-        return polinomio.Length;
+        if(k<0) throw new Exception();
+        else if(k>gradePoly) return 0;
+        return polinomio[k];
+    }
+   
+
+    public new string ToString()
+    {
+        string s = "";
+        for (int i = polinomio.Length - 1; i >= 0; i--)
+        {
+            if(polinomio[i] == 0) continue;
+            s+=form(polinomio.Length - 1,i,polinomio[i]);
+        }
+        if(polinomio.Length>0 && s=="") return "0";
+        return s;
+    }
+    private static string form(int polinomioL,int index,int value)
+    {
+        string s="";
+        if(value==0) return s;
+        else if(polinomioL==index && value>0) return s=$"{value}x^{index}";
+        else if(value>0) return s=$" +{value}x^{index}";
+        
+        return $" +{value}x^{index}";
     }
 
-    // public string ToString()
-    // {
-    //     string s = "";
-    //     for (int i = polinomio.Length - 1; i >= 0; i--)
-    //     {
-    //         if(polinomio[i] == 0) continue;
-            
-    //     }
-    // }
-    
 }
--- Clases/Program.cs
+++ Clases/Program.cs
@@ -2,8 +2,9 @@ class Program
 {
     private static void Main()
     {
-       Poly poly=new Poly([2]);
+       Poly poly=new Poly([2,2,0]);
     
        System.Console.WriteLine(poly.GradePoly);
+       System.Console.WriteLine(poly.ToString());
     }
 }
