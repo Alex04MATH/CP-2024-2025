f6b60cb02cb68fdda5c3d53a385176c4b9c07e76
Alex04 <alexmonterogaray@gmail.com>
2025-02-14 19:48:40 -0500

    Concluid class Fraction and Format Poly

--- Clases/Fraction.cs
+++ Clases/Fraction.cs
@@ -3,44 +3,138 @@ public class Fraction
     private string fraction;
     private int numerator;
     private int denominator;
-    public int GetNumerator{get=>numerator;}
-    public int GetDenominator{get=>denominator;}
-    public Fraction(int numerator,int denominator)
+    private string[] simplify { get => Simplify(numerator, denominator); }
+    public int GetNumerator { get => int.Parse(simplify[0]); }
+    public int GetDenominator { get => int.Parse(simplify[1]); }
+    public string FractionSimplify { get => simplify[2]; }
+    public Fraction(int numerator, int denominator)
     {
-        if(denominator !=0)
+        if (denominator != 0)
         {
-        this.numerator=numerator;
-        this.denominator=denominator;
-        this.fraction=$"{numerator}/{denominator}";
+            this.numerator = numerator;
+            this.denominator = denominator;
+            this.fraction = $"{numerator}/{denominator}";
         }
-        else throw new Exception();    
+        else throw new Exception();
     }
-    public static Fraction operator +(Fraction fraction1,Fraction fraction2)
+    public Fraction(int numerator)
     {
-       int minCM=mcm(fraction1.denominator,fraction2.denominator);
-       int numerator=(fraction1.denominator/minCM)*fraction1.numerator+(fraction2.denominator/minCM)*fraction2.numerator;
-       
-       return new Fraction(numerator,minCM);
+        this.numerator = numerator;
+        this.denominator = 1;
+        this.fraction = $"{numerator}";
+    }
+    public Fraction()
+    {
+        this.numerator = 0;
+        this.denominator = 1;
+        this.fraction = $"{numerator}";
+    }
+    private static string[] Simplify(int numerator, int denominator)
+    {
+        int divisor = MCD(numerator, denominator);
+        String[] arr = new string[3];
+        arr[0] = $"{numerator / divisor}";
+        arr[1] = $"{denominator / divisor}";
+        if (numerator / divisor == 0)
+        {
+            arr[2] = "0";
+            return arr;
+        }
+        if (denominator / divisor == 1)
+        {
+            arr[2] = $"{numerator / divisor}";
+            return arr;
+        }
+        arr[2] = $"{numerator / divisor}/{denominator / divisor}";
+        return arr;
+    }
+    public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+    {
+        int minCM = mcm(fraction1.denominator, fraction2.denominator);
+        int numerator = (fraction1.denominator / minCM) * fraction1.numerator + (fraction2.denominator / minCM) * fraction2.numerator;
+
+        return new Fraction(numerator, minCM);
+    }
+
+    public static Fraction operator *(int num, Fraction fraction)
+    {
+        return new Fraction(num * fraction.numerator, fraction.denominator);
+    }
+
+    public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+    {
+        fraction2 = -1 * fraction2;
+        fraction1 = fraction1 + fraction2;
+        return new Fraction(fraction1.numerator, fraction2.denominator);
+    }
+    public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+    {
+        return new Fraction(fraction1.numerator * fraction2.numerator, fraction1.denominator * fraction2.denominator);
+    }
+
+    public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+    {
+        return new Fraction(fraction1.numerator * fraction2.denominator, fraction1.denominator * fraction2.numerator);
+    }
+    public static bool operator >(Fraction fraction1, Fraction fraction2)
+    {
+        if (fraction1.GetDenominator > fraction2.GetDenominator) return false;
+        else if (fraction1.GetDenominator == fraction2.GetDenominator)
+        {
+            if (fraction1.GetNumerator > fraction2.GetNumerator) return true;
+            else return false;
+        }
+        return true;
+    }
+
+    public static bool operator <(Fraction fraction1, Fraction fraction2)
+    {
+        return !(fraction1 > fraction2);
+    }
+    public static bool operator ==(Fraction fraction1, Fraction fraction2)
+    {
+        if (fraction1.GetNumerator != fraction2.GetNumerator) return false;
+        if (fraction2.GetDenominator != fraction1.GetDenominator) return false;
+        return true;
+    }
+    public static bool operator !=(Fraction fraction1, Fraction fraction2)
+    {
+        return !(fraction1 == fraction2);
     }
     private static int mcm(int x, int y)
     {
-      if(x==y) return x;
-      if(x==1 || y==1) return Math.Max(x,y);
-      if(x==0 || y==0) return 1;
-      int upDivisor=Math.Max(x,y);
-      int divx=1;
-      int divy=1;
-      for(int i=2;i<=upDivisor;i++)
-      {
-         if(x%i==0)
-         {
-            divx*=i;
-         }
-          if(y%i==0)
-          {
-            divy*=0;
-          }
-      }
-      return divx*divy;
+        if (x == y) return x;
+        if (x == 1 || y == 1) return Math.Max(x, y);
+        if (x == 0 || y == 0) return 1;
+        int upDivisor = Math.Max(x, y);
+        int divx = 1;
+        int divy = 1;
+        for (int i = 2; i <= upDivisor; i++)
+        {
+            if (x % i == 0)
+            {
+                divx *= i;
+            }
+            if (y % i == 0)
+            {
+                divy *= 0;
+            }
+        }
+        return divx * divy;
+    }
+    private static int MCD(int numerator, int denominator)
+    {
+        if (numerator == denominator) return numerator;
+        if ((numerator == 1 || denominator == 1) || (numerator == 0 || denominator == 0)) return 1;
+        int mcd = 1;
+        int min = Math.Min(numerator, denominator);
+        for (int i = 2; i <= min; i++)
+        {
+            if (numerator % i == 0 && denominator % i == 0)
+            {
+                mcd *= i;
+            }
+        }
+        return mcd;
     }
 }
--- Clases/Poly.cs
+++ Clases/Poly.cs
@@ -7,7 +7,7 @@ class Poly
     private int[] polinomio;
     public int[] Polinomio { get => polinomio; }
     private int gradePoly;
-    
+
     public int GradePoly
     {
         get => gradePoly;
@@ -32,207 +32,207 @@ class Poly
     private int Grade(int[] polinomio)
     {
         int pos = polinomio.Length - 1;
-        if (polinomio[pos] != 0) return polinomio.Length-1;
-        else if(pos-1>=0)   
-            for (int i = pos=1; i >= 0; i--)
+        if (polinomio[pos] != 0) return polinomio.Length - 1;
+        else if (pos - 1 >= 0)
+            for (int i = pos = 1; i >= 0; i--)
             {
                 if (polinomio[i] != 0) return i;
-                 
+
             }
-        
+
         return 0;
     }
     public int GetCoef(int k)
     {
-        if(k<0) throw new Exception();
-        else if(k>gradePoly) return 0;
+        if (k < 0) throw new Exception();
+        else if (k > gradePoly) return 0;
         return polinomio[k];
     }
-   
+
 
     public new string ToString()
     {
         string s = "";
-        int antvalue=0;
+        int antvalue = 0;
         for (int i = polinomio.Length - 1; i >= 0; i--)
         {
-            if(polinomio[i] == 0) continue;
-            s+=form(i,polinomio[i],antvalue);
-            antvalue=polinomio[i];
+            if (polinomio[i] == 0) continue;
+            s += form(i, polinomio[i], antvalue);
+            antvalue = polinomio[i];
         }
-        if(polinomio.Length>0 && s=="") return "0";
+        if (polinomio.Length > 0 && s == "") return "0";
         return s;
     }
-    private static string form(int index,int value,int antvalue)
+    private static string form(int index, int value, int antvalue)
     {
-        if(value==0) return "";  
-        
-        return $"{antvalueString(value,antvalue)}{exponencialString(index)}";
-    }
-    private static string antvalueString(int value,int antvalue)
-    {
-       if(antvalue==0)
-       {
-         if(value>0) return $"{value}";
-       }
-       if(value>0) return $"+{value}";
+        if (value == 0) return "";
 
-       return $"{value}";
+        return $"{antvalueString(value, antvalue)}{exponencialString(index)}";
+    }
+    private static string antvalueString(int value, int antvalue)
+    {
+        if (antvalue == 0)
+        {
+            if (value > 0) return $"{value}";
+        }
+        if (value > 0) return $"+{value}";
+
+        return $"{value}";
     }
     private static string exponencialString(int index)
-    { 
-        if(index==0) return "";
-        else if(index==1) return "x";
+    {
+        if (index == 0) return "";
+        else if (index == 1) return "x";
         return $"x^{index}";
     }
-    public static Poly operator +(Poly poly1,Poly poly2)
+    public static Poly operator +(Poly poly1, Poly poly2)
     {
-        int a=poly1.polinomio.Length;
-        int b=poly2.polinomio.Length;
-        int[]arr=new int[Math.Max(a,b)];
-        for(int i=0;i<arr.Length;i++)
+        int a = poly1.polinomio.Length;
+        int b = poly2.polinomio.Length;
+        int[] arr = new int[Math.Max(a, b)];
+        for (int i = 0; i < arr.Length; i++)
         {
-            if(i>=a)
+            if (i >= a)
             {
-                arr[i]=poly2.polinomio[i];
-                continue;
-            }      
-            if(i>=b)
-            {
-                arr[i]=poly1.polinomio[i];
+                arr[i] = poly2.polinomio[i];
                 continue;
             }
-            arr[i]=poly1.polinomio[i]+poly2.polinomio[i];
+            if (i >= b)
+            {
+                arr[i] = poly1.polinomio[i];
+                continue;
+            }
+            arr[i] = poly1.polinomio[i] + poly2.polinomio[i];
         }
         return new Poly(arr);
     }
-    public static Poly operator *(Poly poly1,Poly poly2)
+    public static Poly operator *(Poly poly1, Poly poly2)
     {
-        int a=poly1.polinomio.Length-1;
-        int b=poly2.polinomio.Length-1;
-        int[]arr=new int[a+b+1];
-        for(int i=0;i<=a;i++)
+        int a = poly1.polinomio.Length - 1;
+        int b = poly2.polinomio.Length - 1;
+        int[] arr = new int[a + b + 1];
+        for (int i = 0; i <= a; i++)
         {
-            for(int j=0;j<=b;j++)
+            for (int j = 0; j <= b; j++)
             {
-                arr[i+j]+=poly1.polinomio[i]*poly2.polinomio[j];
+                arr[i + j] += poly1.polinomio[i] * poly2.polinomio[j];
             }
         }
         return new Poly(arr);
     }
-    public static Poly operator *(int x,Poly poly)
+    public static Poly operator *(int x, Poly poly)
     {
-        int[] arr=new int[poly.polinomio.Length];
-        for(int i=0;i<poly.polinomio.Length;i++)
+        int[] arr = new int[poly.polinomio.Length];
+        for (int i = 0; i < poly.polinomio.Length; i++)
         {
-            arr[i]=x*poly.polinomio[i];
+            arr[i] = x * poly.polinomio[i];
         }
         return new Poly(arr);
     }
     public static bool operator ==(Poly poly1, Poly poly2)
     {
-        if(poly1.polinomio.Length != poly2.polinomio.Length) return false;
-        for(int i=0;i<poly1.polinomio.Length;i++)
+        if (poly1.polinomio.Length != poly2.polinomio.Length) return false;
+        for (int i = 0; i < poly1.polinomio.Length; i++)
         {
-            if(poly1.polinomio[i] != poly2.polinomio[i]) return false;
+            if (poly1.polinomio[i] != poly2.polinomio[i]) return false;
         }
         return true;
     }
     public static bool operator !=(Poly poly1, Poly poly2)
     {
-        return !(poly1==poly2);
+        return !(poly1 == poly2);
     }
     public static bool operator >(Poly poly1, Poly poly2)
     {
-        if(poly1.polinomio.Length>poly2.polinomio.Length) return true;
-        if(poly1.polinomio.Length<poly2.polinomio.Length) return false;
-        for(int i=0;i<poly1.polinomio.Length;i++)
+        if (poly1.polinomio.Length > poly2.polinomio.Length) return true;
+        if (poly1.polinomio.Length < poly2.polinomio.Length) return false;
+        for (int i = 0; i < poly1.polinomio.Length; i++)
         {
-            if(poly1.polinomio[i]<poly2.polinomio[i]) return false;
+            if (poly1.polinomio[i] < poly2.polinomio[i]) return false;
         }
-        return true && (poly1!=poly2);
+        return true && (poly1 != poly2);
     }
-    public static bool operator <(Poly poly1,Poly poly2)
+    public static bool operator <(Poly poly1, Poly poly2)
     {
-        return !(poly1>poly2) && (poly1!=poly2);
+        return !(poly1 > poly2) && (poly1 != poly2);
     }
-    public static bool operator >=(Poly poly1,Poly poly2)
+    public static bool operator >=(Poly poly1, Poly poly2)
     {
-        return (poly1>poly2) || (poly1==poly2);
+        return (poly1 > poly2) || (poly1 == poly2);
     }
-    public static bool operator <=(Poly poly1,Poly poly2)
+    public static bool operator <=(Poly poly1, Poly poly2)
     {
-        return (poly1<poly2) || (poly1==poly2);
+        return (poly1 < poly2) || (poly1 == poly2);
     }
-    
-    public static Poly operator /(Poly poly1,Poly poly2)
+
+    public static Poly operator /(Poly poly1, Poly poly2)
     {
         //implementar Exeption cuando poly2.grade ==0
-        int _newpolygrade=poly1.polinomio.Length-poly2.gradePoly;
-        int[]arr=new int[_newpolygrade+1];
-        int i=poly1.gradePoly;
-        for(;i>=poly2.gradePoly;i--)
+        int _newpolygrade = poly1.polinomio.Length - poly2.gradePoly;
+        int[] arr = new int[_newpolygrade + 1];
+        int i = poly1.gradePoly;
+        for (; i >= poly2.gradePoly; i--)
         {
-          int division=poly1.polinomio[i]/poly2.polinomio[poly2.gradePoly];
-          int index=i-poly2.gradePoly;
-          int resto=poly1.polinomio[i]%poly2.polinomio[poly2.gradePoly];
-          arr[index]=division;
-          Poly polyx=calculateMonicPoly(index);
-          poly1+=(-division)*(poly2*polyx)+(-1*restoIntDivisor(poly2.gradePoly,resto)*polyx);
+            int division = poly1.polinomio[i] / poly2.polinomio[poly2.gradePoly];
+            int index = i - poly2.gradePoly;
+            int resto = poly1.polinomio[i] % poly2.polinomio[poly2.gradePoly];
+            arr[index] = division;
+            Poly polyx = calculateMonicPoly(index);
+            poly1 += (-division) * (poly2 * polyx) + (-1 * restoIntDivisor(poly2.gradePoly, resto) * polyx);
         }
-        
+
         return new Poly(arr);
     }
     private static Poly calculateMonicPoly(int index)
     {
-        int[]arr=new int[index+1];
-        arr[index]=1;
+        int[] arr = new int[index + 1];
+        arr[index] = 1;
         return new Poly(arr);
     }
-    private static Poly restoIntDivisor(int index,int value)
+    private static Poly restoIntDivisor(int index, int value)
     {
-        int[]arr=new int[index+1];
-        arr[index]=value;
+        int[] arr = new int[index + 1];
+        arr[index] = value;
         return new Poly(arr);
     }
-    public static Poly operator %(Poly poly1,Poly poly2)
+    public static Poly operator %(Poly poly1, Poly poly2)
     {
         //implementar Exeption cuando poly2.grade ==0
         int[] arr;
-        int i=poly1.gradePoly;
-        for(;i>=poly2.gradePoly;i--)
+        int i = poly1.gradePoly;
+        for (; i >= poly2.gradePoly; i--)
         {
-          int division=poly1.polinomio[i]/poly2.polinomio[poly2.gradePoly];
-          int index=i-poly2.gradePoly;
-          int resto=poly1.polinomio[i]%poly2.polinomio[poly2.gradePoly];
-          Poly polyx=calculateMonicPoly(index);
-          poly1+=(-division)*(poly2*polyx)+(-1*restoIntDivisor(poly2.gradePoly,resto)*polyx);
+            int division = poly1.polinomio[i] / poly2.polinomio[poly2.gradePoly];
+            int index = i - poly2.gradePoly;
+            int resto = poly1.polinomio[i] % poly2.polinomio[poly2.gradePoly];
+            Poly polyx = calculateMonicPoly(index);
+            poly1 += (-division) * (poly2 * polyx) + (-1 * restoIntDivisor(poly2.gradePoly, resto) * polyx);
         }
-        arr=poly1.polinomio;
+        arr = poly1.polinomio;
         return new Poly(arr);
     }
     public static Poly Dx(Poly poly1)
     {
-      if(poly1.gradePoly==0) return new Poly([0]);
-      int[] arr=new int[poly1.polinomio.Length-1];
-      for(int i=arr.Length-1,j=poly1.polinomio.Length-1;i>=0;i--,j--)
-      {
-        if(poly1.polinomio[j]==0)
+        if (poly1.gradePoly == 0) return new Poly([0]);
+        int[] arr = new int[poly1.polinomio.Length - 1];
+        for (int i = arr.Length - 1, j = poly1.polinomio.Length - 1; i >= 0; i--, j--)
         {
-            continue;
+            if (poly1.polinomio[j] == 0)
+            {
+                continue;
+            }
+            arr[i] = poly1.polinomio[j] * j;
         }
-        arr[i]=poly1.polinomio[j]*j;
-      }
-      return new Poly(arr);
+        return new Poly(arr);
     }
-    public static int EvaluateX(Poly poly,int value)
+    public static int EvaluateX(Poly poly, int value)
     {
-        int evaluate=0;
-        if(poly.gradePoly==0 && poly.polinomio.Length !=0) return poly.polinomio[0];
-        if(poly.polinomio.Length==0) return 0; 
-        for(int i=0;i<poly.polinomio.Length;i++)
+        int evaluate = 0;
+        if (poly.gradePoly == 0 && poly.polinomio.Length != 0) return poly.polinomio[0];
+        if (poly.polinomio.Length == 0) return 0;
+        for (int i = 0; i < poly.polinomio.Length; i++)
         {
-            evaluate+=poly.polinomio[i]*(int)Math.Pow(value,i);
+            evaluate += poly.polinomio[i] * (int)Math.Pow(value, i);
         }
         return evaluate;
     }
