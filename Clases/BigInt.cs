a33020fde2db550408921ef987b1e34685dd2bd5
Alex04 <alexmonterogaray@gmail.com>
2025-03-01 11:42:19 -0500

    Add Operations: -,*,/.%

--- Clases/BigInt.cs
+++ Clases/BigInt.cs
@@ -8,15 +8,32 @@ public class BigInt
     private char[] digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     private int Length;
     private string numInvert;
-    public string GetString{get=>num;}
+    public string GetString { get => num; }
     public BigInt(string num)
     {
         if (!validateNum(num)) new NotImplementedException();
-        this.num = num;
-        this.numInvert= invertString(num);
-        Length=num.Length;
+        this.num = CreateBigInt(num);
+        this.numInvert = invertString(this.num);
+        Length = num.Length;
+    }
+    public BigInt()
+    {
+        this.num = "0";
+        this.numInvert = num;
+        Length = num.Length;
     }
 
+    private static string CreateBigInt(string num)
+    {
+        string newNum = "";
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (num[i] != '0') newNum += num[i];
+        }
+
+        return newNum == "" ? "0" : newNum;
+    }
+    
     private static bool validateNum(string num)
     {
         char[] digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -26,54 +43,108 @@ public class BigInt
         }
         return true;
     }
+    
     private static string invertString(string str)
     {
-       var invert=str.ToCharArray();
-       Array.Reverse(invert);
-       str=new string(invert);
-       return str;
+        var invert = str.ToCharArray();
+        Array.Reverse(invert);
+        str = new string(invert);
+        return str;
     }
+    
     private static int Value(Char num)
     {
-        int val=0;
+        int val = 0;
         char[] digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-        for(int i = 0;i<digit.Length;i++)
+        for (int i = 0; i < digit.Length; i++)
         {
-            if(num==digit[i])
+            if (num == digit[i])
             {
                 val = i;
                 break;
             }
         }
-     return val;
+        return val;
     }
+    
+    private static int Value(string num)
+    {
+        int value = 0;
+        int Length = num.Length;
+        for (int i = 0; i < Length; i++)
+        {
+            value += (int)Math.Pow(10, (Length - 1) - i) * Value(num[i]);
+        }
+        return value;
+    }
+    
     public static BigInt operator +(BigInt bigInt1, BigInt bigInt2)
     {
-        string num="";
-        int arras=0;
-        for(int i=0;i<Math.Max(bigInt1.Length, bigInt2.Length);i++)
+        string num = "";
+        int arras = 0;
+        for (int i = 0; i < Math.Max(bigInt1.Length, bigInt2.Length); i++)
         {
-           if(i>=bigInt1.Length)
-           {
-             int value1=Value(bigInt2.numInvert[i])+arras;
-             num+=value1%10;
-             arras=value1/10;
-             continue;
-           }
-           if(bigInt2.Length<=i)
-           {
-             int value2=Value(bigInt1.numInvert[i])+arras;
-             num+=value2%10;
-             arras=value2/10;
-             continue;
-           }
-             int value=Value(bigInt1.numInvert[i])+Value(bigInt2.numInvert[i])+arras;
-             num+=value%10;
-             arras=value/10;
+            if (i >= bigInt1.Length)
+            {
+                int value1 = Value(bigInt2.numInvert[i]) + arras;
+                num += value1 % 10;
+                arras = value1 / 10;
+                continue;
+            }
+            if (bigInt2.Length <= i)
+            {
+                int value2 = Value(bigInt1.numInvert[i]) + arras;
+                num += value2 % 10;
+                arras = value2 / 10;
+                continue;
+            }
+            int value = Value(bigInt1.numInvert[i]) + Value(bigInt2.numInvert[i]) + arras;
+            num += value % 10;
+            arras = value / 10;
         }
-        if(arras==1) num+=1;
-        num=invertString(num);
-       return new BigInt(num);
+        if (arras == 1) num += 1;
+        num = invertString(num);
+        return new BigInt(num);
+    }
+    
+    public static BigInt operator -(BigInt bigInt1, BigInt bigInt2)
+    {
+        if (bigInt1.Length < bigInt2.Length) throw new Exception();
+        if (bigInt1.Length == bigInt2.Length && (Value(bigInt1.num[0])) < Value(bigInt2.num[0])) throw new Exception();
+        string num = "";
+        for (int i = 0; i < bigInt1.Length; i++)
+        {
+            num += Value(bigInt1.numInvert[i]) - Value(bigInt2.numInvert[i]);
+        }
+        num = invertString(num); //cambiar el calcúlo en for desde el ultimo para ejecutar mas rápido con StringBuilder
+        return new BigInt(num);
+    }
+    
+    public static BigInt operator *(BigInt bigInt1, BigInt bigInt2)
+    {
+        int val = Value(bigInt1.num);
+        int val1 = Value(bigInt2.num);
+        string num = $"{val * val1}";
+
+        return new BigInt(num);
+    }
+    
+    public static BigInt operator /(BigInt bigInt1, BigInt bigInt2)
+    {
+        int val = Value(bigInt1.num);
+        int val1 = Value(bigInt2.num);
+        string num = $"{val / val1}";
+
+        return new BigInt(num);
+    }
+
+    public static BigInt operator %(BigInt bigInt1, BigInt bigInt2)
+    {
+        int val = Value(bigInt1.num);
+        int val1 = Value(bigInt2.num);
+        string num = $"{val % val1}";
+
+        return new BigInt(num);
     }
 }
