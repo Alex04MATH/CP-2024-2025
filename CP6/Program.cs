2f2b15d8ac8539506263a79f3db614b361b251e1
Alex04 <alexmonterogaray@gmail.com>
2025-04-09 04:02:17 -0400

    End CP6

--- CP6/Program.cs
+++ CP6/Program.cs
@@ -6,33 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Stopwatch time = new Stopwatch();
-        time.Start();
-        // Console.WriteLine("Hello, World!");
-        // int [] uno = BubbleSort([23,45,9238,1,3,30,29,0,-1]);
-        // for (int i = 0; i < uno.Length; i++)
-        // {
-        //     Console.WriteLine(uno[i]);
-        // }
-        // Console.WriteLine(CualNumber(4,[4, 2, 4, 5, 6, 2, 7]));
-        int[] a = Solution.BubbleSort([23, 45, 9238, 1, 3, 30, 29, 0, -1]);
-        int[] b = Solution.SelectionSort([23, 23, 9238, 1, 1, 1, 29, 0, -1]);
-        int c = Solution.MinDiff([4, 9, 1, 32, 13, 6]);
-        Console.WriteLine(Solution.NumberingSystem(['a', 'b'], "a"));
-        Console.WriteLine(Solution.ReverseNumberingSystem(['a', 'b'], 0));
-        Console.WriteLine(Solution.BinarySearch(a, 30));
-        Console.WriteLine(c);
-        Console.WriteLine(Solution.Consecutive(b, 3));
-        Console.WriteLine(Solution.MaxSumSub([0, -1, 0, -2, 0, -3]));
-        Console.WriteLine(Solution.CountInversions([1, 2, 3, 4, 5]));
-        Console.WriteLine(Solution.Anagrams("CAsA1", "S4CA1"));
-        time.Stop();
-        System.Console.WriteLine(Solution.MaxSubSetAnagram(["amor", "saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora",]));
-        System.Console.WriteLine(Solution.MaxSubSetAnagram(["amor", "saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora","saca", "casa", "mora", "mora","mora"]));
-        System.Console.WriteLine(time.ElapsedMilliseconds);
-        Console.WriteLine(Solution.Occurrences("mississippi","s"));
-        System.Console.WriteLine(Solution.HowManyNumbers(1,[7, 7, 7, 7, 8, 9, 10, 8]));
-        System.Console.WriteLine(Solution.ErastotonSieve(100));
-        System.Console.WriteLine(Solution.ReverseSubArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],2));
+        
     }
 }
--- CP6/Solution(10-14).cs
+++ CP6/Solution(10-14).cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Formats.Asn1;
 
 public partial class Solution
 {
@@ -25,34 +27,100 @@ public partial class Solution
         return true;
     }
 
-    public static int[] ReverseSubArray(int[] items,int k)
+    public static int[] ReverseSubArray(int[] items, int k)
     {
-       
-       int tall=k-1;
-       for (int i = 0,j=tall;i<items.Length && j<items.Length;i=++j,j=i+tall)
-       {
-           for(int pointer=i,pointer1=j;j-(j-i)/2<=pointer1 && j-(j-i)/2>=pointer;pointer++,pointer1--)
-           {
-             int temp=items[pointer];
-             items[pointer] = items[pointer1];
-             items[pointer1] = temp;
-           }
-       }
-       return items;
+
+        int tall = k - 1;
+        for (int i = 0, j = tall; i < items.Length && j < items.Length; i = ++j, j = i + tall)
+        {
+            for (int pointer = i, pointer1 = j; j - (j - i) / 2 <= pointer1 && j - (j - i) / 2 >= pointer; pointer++, pointer1--)
+            {
+                int temp = items[pointer];
+                items[pointer] = items[pointer1];
+                items[pointer1] = temp;
+            }
+        }
+        return items;
     }
 
-    public static int[] SortbyProximity(int[] arr,int pivot)
+    public static int[] SortbyProximity(int[] arr, int pivot)
     {
-        bool[] mask=new bool[arr.Length];
-        for(int i=0;i<arr.Length;i++)
+        bool[] isNegative = new bool[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
         {
-            if(arr[i]>=0) mask[i]=true;
-            arr[i]-=pivot;
-            Math.Abs(arr[i]);
+            arr[i] -= pivot;
+            if (arr[i] < 0) isNegative[i] = true;
+            arr[i] = Math.Abs(arr[i]);
         }
 
-        return 
+        return Sort(arr, isNegative, pivot);
     }
-} 
-    
-    
+    private static int[] Sort(int[] arr, bool[] isNegative, int pivot)
+    {
+        (int, bool) index, index1;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                index = (arr[i], isNegative[i]);
+                index1 = (arr[j], isNegative[j]);
+                if (index.Item1 >= index1.Item1)
+                {
+                    if (index.Item1 == index1.Item1 && (index.Item2 != index1.Item2 && index.Item2 == false))
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = arr[i];
+                        isNegative[i] = true;
+                        isNegative[j] = false;
+                    }
+                    else
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+
+                        bool temp1 = isNegative[i];
+                        isNegative[i] = isNegative[j];
+                        isNegative[j] = temp1;
+
+                    }
+                }
+            }
+        }
+        return sumArr(arr, isNegative, pivot);
+    }
+    private static int[] sumArr(int[] arr, bool[] isNegative, int pivot)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (isNegative[i]) arr[i] = -arr[i] + pivot;
+            else arr[i] += pivot;
+        }
+        return arr;
+    }
+
+    public static int[] SolveDistributionChocolate(int[] arr, int student)
+    {
+       int sum=0;
+       for (int i = 0;i<arr.Length;i++)
+       {
+           sum+=arr[i];
+       }
+       if(sum<student) return new int[] {};
+
+       int[] DistributionByStudent=new int[student];
+       int div=sum/student;
+       int rest=sum%student;
+       for (int i=0;i<DistributionByStudent.Length;i++)
+       {
+         int temp=0;
+         if(rest<=0) rest = 0;
+         else temp=1;
+         DistributionByStudent[i]=div+temp;
+         rest--;
+       }
+
+       return DistributionByStudent;
+    }
+}
