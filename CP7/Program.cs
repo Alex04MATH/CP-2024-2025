bc9b337b19506e89d58468c4e57de8bdfb02a34a
Alex04 <alexmonterogaray@gmail.com>
2025-04-09 22:23:16 -0400

    modify files and Add new Solution CP7

--- CP7/Program.cs
+++ CP7/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        Stopwatch stopwatch= new Stopwatch();
+        stopwatch.Start();
         Console.WriteLine("Hello, World!");
-        int[,] a = new int[,] { { 2, 3, 2, 1 }, { 2, 3, 4, 5 } };
-        int[,] b = SumaDeMatrices(a, a);
+        int[,] a = new int[,] { { 2, 3 ,4}, { 3, 2,5 },{4,5,2} };
+        int[,] b = Solution.SumOfMatrix(a, a);
         foreach (int i in b)
         {
             Console.WriteLine(i);
         }
-        int[] h = Espiral(a);
-        foreach (var e in h)
+
+        foreach (var e in a)
         {
             Console.Write(e + ", ");
         }
+        stopwatch.Stop();
+        System.Console.WriteLine();
+        System.Console.WriteLine(stopwatch.ElapsedTicks);
+       System.Console.WriteLine(Solution.MultiplyMatrix(a,a));
+       System.Console.WriteLine(Solution.Trace(a));
+       int[,] c = new int[,] { { 5, 7 ,2}, { 6, 0,3 },{1,4,5} };
     }
 }
---
+++ CP7/Solution(1-7).cs
@@ -0,0 +1,125 @@
+public partial class Solution
+{
+    public static int[,] SumOfMatrix(int[,] matrix1, int[,] matrix2)
+    {
+        int[,] matrix3 = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.GetLength(1); j++)
+            {
+                matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+        return matrix3;
+    }
+
+    public static int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+    {
+        if (matrix1.GetLength(1) != matrix2.GetLength(0)) return new int[,] { };
+        int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                int[] Row = row(matrix1, i);
+                int[] Column = column(matrix2, j);
+                resultMatrix[i, j] = Num(Row, Column);
+            }
+        }
+        return resultMatrix;
+    }
+    private static int[] row(int[,] matrix, int num)
+    {
+        int[] Rows = new int[matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            Rows[i] = matrix[num, i];
+        }
+        return Rows;
+    }
+    private static int[] column(int[,] matrix, int num)
+    {
+        int[] Columns = new int[matrix.GetLength(0)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Columns[i] = matrix[i, num];
+        }
+        return Columns;
+    }
+    private static int Num(int[] row, int[] col)
+    {
+        int num = 0;
+        for (int i = 0; i < row.Length; i++)
+        {
+            num += row[i] * col[i];
+        }
+        return num;
+    }
+
+    public static int Trace(int[,] matrix)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1)) return -1;
+        int sum = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, i];
+        }
+
+        return sum;
+    }
+    public static int[,] HandoverMatrix(int[,] matrix)
+    {
+        int[,] resultMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                resultMatrix[j, i] = matrix[i, j];
+            }
+        }
+        return resultMatrix;
+    }
+    public static bool IsSymmetry(int[,] matrix)
+    {
+        bool isSymmetry = true;
+        int val = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = val; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] != matrix[j, i]) return false;
+            }
+            val++;
+        }
+        return isSymmetry;
+    }
+    public static int[,] Cero(int[,] matrix)
+    {
+        List<(int, int)> CoordinateCeros = new List<(int, int)>();
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == 0) CoordinateCeros.Add((i, j));
+            }
+        }
+
+        for (int i = 0; i < CoordinateCeros.Count; i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[CoordinateCeros[i].Item1, j] = 0;
+            }
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                matrix[j, CoordinateCeros[i].Item2] = 0;
+            }
+        }
+        return matrix;
+    }
+}
