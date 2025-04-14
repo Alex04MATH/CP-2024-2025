491b58af9e12ad13bccb2fa03911fb9ede611b16
Alex04 <alexmonterogaray@gmail.com>
2025-04-14 16:15:04 -0400

    Fix Spiral function

--- CP7/Solution(1-7).cs
+++ CP7/Solution(1-7).cs
@@ -129,50 +129,38 @@ public partial class Solution
 
         int[] arr = new int[matrix.GetLength(0) * matrix.GetLength(1)];
 
-       bool[,] mask = new bool[matrix.GetLength(0), matrix.GetLength(1)];
+        bool[,] mask = new bool[matrix.GetLength(0), matrix.GetLength(1)];
 
         (int, int)[] direction = { (0, 1), (1, 0), (0, -1), (-1, 0) };
 
         int x = 0, y = 0;
         int pos = 0;
-        arr[0]=matrix[0,0];
-        mask[0,0]=true;
-        for (int i = 1; i < arr.Length; i++)
+        for (int i = 0; i < arr.Length; i++)
         {
-            x += direction[pos].Item1;
-            y += direction[pos].Item2;
-           if(x>=matrix.GetLength(0))
-           {
-            x-=1;
-            pos=++pos%4;
-            y += direction[pos].Item2;
-           }
-           if(y>=matrix.GetLength(1))
-           {
-             y-=1;
-             pos=++pos%4;
-             x+=direction[pos].Item1;
-           } 
-           if(y<0)
-           {
-            y=0;
-            pos=++pos%4;
-            x+=direction[pos].Item1;
-           }
-           if(x<0)
-           {
-            x=0;
-            pos=++pos%4;
-            y+=direction[pos].Item2;
-           }
-            if (mask[x, y])
-            {
-                pos=++pos%4;
-                x += direction[pos].Item1;
-                y += direction[pos].Item2;
-            }
-            mask[x,y]=true;
             arr[i] = matrix[x, y];
+            mask[x, y] = true;
+            int row = x + direction[pos].Item1;
+            int col = y + direction[pos].Item2;
+
+            if (row < 0 || row >= matrix.GetLength(0) || col < 0 || col >= matrix.GetLength(1))
+            {
+                pos = (++pos) % 4;
+
+                row = x + direction[pos].Item1;
+
+                col = y + direction[pos].Item2;
+
+            }
+            if (mask[row, col])
+            {
+                pos = (++pos) % 4;
+
+                row = x + direction[pos].Item1;
+
+                col = y + direction[pos].Item2;
+            }
+            x = row;
+            y = col;
         }
         return arr;
     }
